4116c6e7155e0713665b2d0770aaade28d9881f1
AshwiniP-Hub <ashwinivpatil4@gmail.com>
2025-01-18 22:14:52 +0530

    update backend
    cors problem solve

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Enable CORS for all origins or specify your frontend URL
+app.use(cors({
+  origin: 'https://todo-list-web-rho.vercel.app',  // Allow only your frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
+
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
