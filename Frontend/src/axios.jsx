2bf3c60e653b09eb5cdba431545406e6c72f511b
Ashwini Patil <ashwinivpatil4@gmail.com>
2025-01-18 20:56:02 +0530

    Update axios.jsx

--- Frontend/src/axios.jsx
+++ Frontend/src/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/todos",
+  // baseURL: "http://localhost:5000/todos",
+  baseURL: "https://todo-list-mfz7.onrender.com/todos",
 });
 
 export default instance;
