22e861ff02f7784c47af5002bc965c4f5fd9dc45
Ashwini Patil <ashwinivpatil4@gmail.com>
2025-01-27 17:05:18 +0530

    Update Signup.jsx
    https://todo-list-mfz7.onrender.com
    update with render link

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -4,10 +4,9 @@ import { Link, useLocation, useNavigate } from 'react-router-dom'
 import Login from './Login'
 import axios from 'axios'
 import toast from 'react-hot-toast';
-// import { Navigate } from "react-router-dom"
 
 function Signup() {
-  //signup karene ke bad hum chahate he ki hum sidha home page pe jaye
+
   const location=useLocation();
   const navigate=useNavigate();
   const from=location.state?.from?.pathname || "/"
@@ -16,16 +15,16 @@ function Signup() {
     handleSubmit,
     formState: { errors },
   } = useForm()
-  //  backend ko add karate samay yah line hatayenge
-  // const onSubmit = (data) => console.log(data)
+  
   const onSubmit =async (data) => {
     const userInfo ={
       fullname:data.fullname,
       email:data.email,
       password:data.password,
     }
-
-    await axios.post("http://localhost:5000/user/signup",userInfo)
+ 
+    await axios.post("https://todo-list-mfz7.onrender.com/user/signup",userInfo)
+    // await axios.post("http://localhost:5000/user/signup",userInfo)
     .then((res)=>{
       console.log(res.data)
       if(res.data){
